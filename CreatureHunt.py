e96f30a34b799b1e4b883646bf9b681c1797e325
QuantuCodes <ridera.2125@gmail.com>
2026-08-11 00:19:34 +0100

    NewFeature: Constant Shiny Probability implemented. NewFeature: JSON entries may be empty. NewFeature: Outputs respective JSON entry message. NewFeature: If P(Shiny) is hit, then shiny message displayed.

--- CreatureHunt.py
+++ CreatureHunt.py
@@ -10,5 +10,13 @@ with open("Creatures.JSON", "r", encoding="utf-8") as file:
 creatureName = random.choice(list(data.keys()))
 chosen = data[creatureName]
 
+#Special "shiny message" based on conditional probability
+isShiny = random.random() < (1/4096)
+
 #output respective message for chosen creature
-print(chosen["message"])
+if isShiny and ("shinyMessage" in chosen):
+    print(chosen["shinyMessage"])
+elif "message" in chosen:
+    print(chosen["message"])
+else:
+    print("You have caught", creatureName)
