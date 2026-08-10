d9a3b13ac8edb2781840a97ebfba2f585b86c3e8
QuantuCodes <ridera.2125@gmail.com>
2026-08-10 23:21:23 +0100

    NewFeature: Randomly selects one entry from the JSON, outputting its respective message.

--- CreatureHunt.py
+++ CreatureHunt.py
@@ -2,7 +2,13 @@ import json
 import random
 #import sys potentially
 
+#locate + open JSON data file in read
 with open("Creatures.JSON", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-print(data)
+#select random creature from JSON (in O(1); dictionary)
+creatureName = random.choice(list(data.keys()))
+chosen = data[creatureName]
+
+#output respective message for chosen creature
+print(chosen["message"])
